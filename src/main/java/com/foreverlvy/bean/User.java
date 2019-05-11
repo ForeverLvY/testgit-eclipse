5f0a61e4559d24cc4a1344ad1cc4d92d8e0cfd21
ForeverLvY <ForeverLvY@163.com>
2019-05-11 13:30:23 +0800

    eclipse2

--- src/main/java/com/foreverlvy/bean/User.java
+++ src/main/java/com/foreverlvy/bean/User.java
@@ -4,7 +4,7 @@ public class User {
 	
 	public static void main(String[] args) {
 		System.out.println("1111111111112222");
-		System.out.println("hot fix");
+		System.out.println("hot fix eclipse");
 	}
 
 }
