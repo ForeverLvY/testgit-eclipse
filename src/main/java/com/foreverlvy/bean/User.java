e9a1e00b8d02eb8c40b9186ed733bf2e5a8d6956
ForeverLvY <ForeverLvY@163.com>
2019-05-11 11:38:00 +0800

    eidt user

--- src/main/java/com/foreverlvy/bean/User.java
+++ src/main/java/com/foreverlvy/bean/User.java
@@ -3,7 +3,7 @@ package com.foreverlvy.bean;
 public class User {
 	
 	public static void main(String[] args) {
-		System.out.println("111111111111");
+		System.out.println("1111111111112222");
 	}
 
 }
